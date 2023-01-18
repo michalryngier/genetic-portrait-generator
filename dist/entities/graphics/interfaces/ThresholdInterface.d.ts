fa92c79a6c616cf4ce9c6266df88c762ec56fcdd
michalryngier <m.ryngier@contina.pl>
2023-01-18 13:34:02 +0100

    feat: generated files

---
+++ dist/entities/graphics/interfaces/ThresholdInterface.d.ts
@@ -0,0 +1,8 @@
+interface ThresholdInterface {
+    xMin: number;
+    xMax: number;
+    yMin: number;
+    yMax: number;
+}
+export default ThresholdInterface;
+//# sourceMappingURL=ThresholdInterface.d.ts.map
